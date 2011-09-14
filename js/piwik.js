0b4babda6e481dd80cd5f38adc7e936b4ba867b3
Matthew Tan Creti <mtancret@purdue.edu>
2011-09-14 12:30:04 -0400

    Changed back piwik script.

--- js/piwik.js
+++
@@ -1,8 +0,0 @@
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://tancreti.net/piwik/" : "http://tancreti.net/piwik/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-
-try {
- var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
- piwikTracker.trackPageView();
- piwikTracker.enableLinkTracking();
-} catch( err ) {}
